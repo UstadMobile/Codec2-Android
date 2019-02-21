ba2308980dc3c212908d7e9bacfa8b40092e3891
Samih Mustafa\Samih <samih@ustadmobile.com>
2019-02-21 18:50:54 +0400

    added prebuilt codec2

--- app/src/main/java/com/ustadmobile/codec2/MainActivity.java
+++ app/src/main/java/com/ustadmobile/codec2/MainActivity.java
@@ -1,8 +1,20 @@
 package com.ustadmobile.codec2;
 
+import android.content.res.AssetManager;
+import android.media.AudioAttributes;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +23,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(Codec2.getBitsSize(34343L));
+        AssetManager asMgr = getApplicationContext().getAssets();
+        int intSize = AudioTrack.getMinBufferSize(16000,
+                AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                AudioFormat.ENCODING_PCM_16BIT);
+
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        AudioTrack track = new AudioTrack(
+                AudioManager.STREAM_MUSIC,
+                16000,
+                AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                intSize,
+                AudioTrack.MODE_STREAM);
+        track.play();
+
+
+        byte[] boutBuf = new byte[2];
+
+        try {
+            InputStream is = asMgr.open("demo.c2");
+
+            long con = Codec2.create(Codec2.CODEC2_MODE_1300);
+            char[] codec2InBuf = new char[Codec2.getBitsSize(con)];
+            byte[] codec2InBufBytes = new byte[codec2InBuf.length];
+            ByteBuffer buffer = ByteBuffer.allocate(Codec2.getSamplesPerFrame(con) * 2 * 2);
+
+            while (is.read(codec2InBufBytes) == codec2InBufBytes.length) {
+                //should be read from stream as character array of this length. The c2dec method reads in as char
+
+                for (int i = 0; i < codec2InBufBytes.length; i++) {
+                    codec2InBuf[i] = (char) codec2InBufBytes[i];
+                }
+                short[] rawAudioOutBuf = new short[Codec2.getSamplesPerFrame(con) * 2];
+
+                Codec2.decode(con, rawAudioOutBuf, codec2InBuf);
+
+                buffer.clear();
+                for (int i = 0; i < rawAudioOutBuf.length; i++) {
+                    buffer.putShort(rawAudioOutBuf[i]);
+                    boutBuf[0] = (byte) (rawAudioOutBuf[i] & 0xFF);
+                    boutBuf[1] = (byte) ((rawAudioOutBuf[i] << 8) & 0xFF);
+//                    bout.write(boutBuf);
+//                    track.write(boutBuf, 0, boutBuf.length);
+                }
+                track.write(buffer,buffer.capacity(),AudioTrack.WRITE_BLOCKING );
+            }
+
+            Codec2.destroy(con);
+
+            System.out.println();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 }
