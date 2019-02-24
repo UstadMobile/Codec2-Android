b641e4eff9db11e0a5f6b643c66078fb7f79cdae
Samih Mustafa\Samih <samih@ustadmobile.com>
2019-02-24 13:05:13 +0400

    c2 files

--- app/src/main/java/com/ustadmobile/codec2/MainActivity.java
+++ app/src/main/java/com/ustadmobile/codec2/MainActivity.java
@@ -32,38 +32,18 @@ public class MainActivity extends AppCompatActivity {
                 intSize,
                 AudioTrack.MODE_STREAM);
         track.play();
-
-
-
-        byte[] boutBuf = new byte[2];
-
         try {
-            InputStream is = asMgr.open("demo.c2");
+            InputStream is = asMgr.open("video1-1300.c2");
+            Codec2Decoder codec2 = new Codec2Decoder(is, Codec2.CODEC2_MODE_1300);
 
-            long con = Codec2.create(Codec2.CODEC2_MODE_1300);
-            char[] codec2InBuf = new char[Codec2.getBitsSize(con)];
-            byte[] codec2InBufBytes = new byte[codec2InBuf.length];
-            ByteBuffer buffer = ByteBuffer.allocate(Codec2.getSamplesPerFrame(con) * 2 * 2);
-            buffer.order(ByteOrder.nativeOrder());
-            while (is.read(codec2InBufBytes) == codec2InBufBytes.length) {
-                //should be read from stream as character array of this length. The c2dec method reads in as char
-                for (int i = 0; i < codec2InBufBytes.length; i++) {
-                    codec2InBuf[i] = (char) codec2InBufBytes[i];
+            while (true) {
+                ByteBuffer buffer = codec2.readFrame();
+                if (buffer != null) {
+                    track.write(buffer.array(), 0, buffer.capacity());
+                } else {
+                    break;
                 }
-                short[] rawAudioOutBuf = new short[Codec2.getSamplesPerFrame(con) * 2];
-
-                Codec2.decode(con, rawAudioOutBuf, codec2InBuf);
-
-                buffer.clear();
-                for (int i = 0; i < rawAudioOutBuf.length; i++) {
-                    buffer.putShort(rawAudioOutBuf[i]);
-                }
-                track.write(buffer.array(), 0, buffer.capacity());
             }
-
-            Codec2.destroy(con);
-
-            System.out.println();
 
 
         } catch (IOException e) {
